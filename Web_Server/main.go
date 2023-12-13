ee584fceb42180897a286f83ba1d93e091553b49
Shashwat Singh <shashwat123student@gmail.com>
2023-12-14 00:27:06 +0530

    Made a super basic web server using go

---
+++ Web_Server/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "Method is not supported", http.StatusNotFound)
+		return
+	}
+	fmt.Fprintf(w, "Hello!")
+}
+
+func main() {
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/hello", helloHandler)
+	fmt.Println("Server Starting at port 8000\n")
+	if err := http.ListenAndServe(":8000", nil); err != nil {
+		log.Fatal(err)
+	}
+}
