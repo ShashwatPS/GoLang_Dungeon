2451a53bf777b7198fba9556e215cf8d1fab798c
Shashwat Singh <shashwat123student@gmail.com>
2024-03-28 02:32:11 +0530

    Created a basic get request which is returning data

--- main.go
+++ main.go
@@ -1,22 +1,38 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-
+	"encoding/json"
 	"github.com/gorilla/mux"
+	"net/http"
 )
+
+func BookHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	title := vars["title"]
+	page := vars["page"]
+
+	responseData := map[string]string{
+		"title": title,
+		"page":  page,
+	}
+
+	jsonResponse, err := json.Marshal(responseData)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(jsonResponse)
+}
 
 func main() {
 	r := mux.NewRouter()
 
-	r.HandleFunc("/books/{title}/page/{page}", func(w http.ResponseWriter, r *http.Request) {
-		vars := mux.Vars(r)
-		title := vars["title"]
-		page := vars["page"]
-
-		fmt.Fprintf(w, "You've requested the book: %s on page %s\n", title, page)
-	}).Methods("GET")
+	r.HandleFunc("/books/{title}/page/{page}", BookHandler).Methods("GET")
 
 	http.ListenAndServe(":3000", r)
 }
