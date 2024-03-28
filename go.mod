c8b559944968dfc50c21aa57a1b325e4055b154f
Shashwat Singh <shashwat123student@gmail.com>
2024-03-28 17:46:51 +0530

    Basic post request done where I am able to save data to the databse

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/GoLang_Dungeon
 go 1.21.3
 
 require (
-	github.com/gorilla/mux v1.8.1 // indirect
-	github.com/steebchen/prisma-client-go v0.35.0 // indirect
+	github.com/gorilla/mux v1.8.1
+	github.com/joho/godotenv v1.5.1
+	github.com/shopspring/decimal v1.3.1
+	github.com/steebchen/prisma-client-go v0.35.0
 )
--- main.go
+++ main.go
@@ -1,19 +1,28 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
+	"fmt"
+	"github.com/GoLang_Dungeon/db"
 	"github.com/gorilla/mux"
 	"net/http"
 )
 
 func BookHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
-	title := vars["title"]
-	page := vars["page"]
+	fname := vars["fname"]
+	lname := vars["lname"]
+	hname := vars["hname"]
+
+	if err := saveToDataBase(fname, lname, hname); err != nil {
+		panic(err)
+	}
 
 	responseData := map[string]string{
-		"title": title,
-		"page":  page,
+		"Fiest Name": fname,
+		"Last Name":  lname,
+		"Hobby":      hname,
 	}
 
 	jsonResponse, err := json.Marshal(responseData)
@@ -29,10 +38,40 @@ func BookHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write(jsonResponse)
 }
 
+func saveToDataBase(fname string, lname string, hobby string) error {
+	client := db.NewClient()
+	if err := client.Prisma.Connect(); err != nil {
+		return err
+	}
+
+	defer func() {
+		if err := client.Prisma.Disconnect(); err != nil {
+			panic(err)
+		}
+	}()
+
+	ctx := context.Background()
+
+	createdUser, err := client.User.CreateOne(
+		db.User.FirstName.Set(fname),
+		db.User.LastName.Set(lname),
+		db.User.Hobby.Set(hobby),
+	).Exec(ctx)
+
+	if err != nil {
+		return err
+	}
+
+	result, _ := json.MarshalIndent(createdUser, "", "  ")
+	fmt.Printf("created post: %s\n", result)
+
+	return nil
+}
+
 func main() {
 	r := mux.NewRouter()
 
-	r.HandleFunc("/books/{title}/page/{page}", BookHandler).Methods("GET")
+	r.HandleFunc("/first/{fname}/last/{lname}/hobby/{hname}", BookHandler).Methods("POST")
 
 	http.ListenAndServe(":3000", r)
 }
